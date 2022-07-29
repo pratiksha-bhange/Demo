8d4a3313d3c64a5cb3548a1b9f87f969d33540fa
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-07-29 22:40:59 +0530

    Pratu

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// UC1-Additon
+Console.WriteLine("Addition use case");
+
+
+// UC2-Substraction
+Console.WriteLine("Substraction use case");
+
+
+// UC3-Multiplication
+Console.WriteLine("Multiplication use case");
+
+
+// UC4-Division
+Console.WriteLine("Division use case");
